67b851bb25a8c46b732b7e0d9a9656fc995bed0b
Wolf Oliver <owolf@specify.io>
2017-04-18 19:26:15 +0200

    first version of a changeset client in the browser
    still to do:
    - process send buffer
    - process receive buffer
    - position caret right

--- index.js
+++ index.js
@@ -8,12 +8,31 @@ var Attribute = require('./models/attribute.js'),
 
 bayeux.attach(server);
 
-bayeux.getClient().subscribe('/changes/uncommited/variable/*', function(message) {
-    console.log('Got a message: ' + message);
+bayeux.getClient().subscribe('/uncommited/changes/variable/*', function(change) {
+    //setTimeout(() => {
+        Attribute.changeVariable(change, (commitedChange) => {
+            console.log(commitedChange);
+            bayeux.getClient().publish('/changes/variable/' + change.variableId, commitedChange);
+        });
+    //}, 1000);
 });
 
-app.get('/concepts/instances', function (req, res) {
-    res.send(attr);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/variables/:id', function (req, res) {
+    console.log('get variable with id ' + req.params.id);
+
+    Attribute.getVariable({variableId: req.params.id}, (result) => {
+        if(result instanceof Error) {
+            res.status(404).send({error: result.message});
+        } else {
+            res.send(result);
+        }
+    })
 });
 
 server.listen(3000);
