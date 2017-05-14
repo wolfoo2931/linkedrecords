58b4d58fef053c8b8ae533e5485c132d0eb6b5fb
Wolf Oliver <owolf@specify.io>
2017-05-14 22:49:49 +0200

    position caret after content has been updated

--- js_sdk/caret.js
+++ js_sdk/caret.js
@@ -6,33 +6,70 @@ var Caret = function() {
 
 Caret.prototype = {
 
-    // endChild is optional, if given it must be child element
-    // of the element passed in the first parameter
-    //TODO: evaluate whether innerText can help to simpify this (seems not to be helpfull because it does not repsect img tags)
+    _getElementsContentLengthIncrement(element) {
+        if(['BR', 'IMG'].includes(element.tagName)) return 1;
+        if(!element.length) return 0;
+        return element.length;
+    },
+
     _getContentLengthOfDOMElement: function(element, endChild) {
+        var treeWalker = document.createTreeWalker(element),
+            currentEl,
+            length = 0;
 
-        var stopCounting = false;
+        while(currentEl = treeWalker.currentNode) {
+            if(currentEl === endChild) return length;
 
-        function countContentLengthOfChildren(childEl, endChild) {
-            var result = 0;
+            length += this._getElementsContentLengthIncrement(currentEl);
 
-            if(childEl === endChild) stopCounting = true;
-            if(stopCounting) return 0;
-
-            if(['BR', 'IMG'].includes(childEl.tagName)) return 1;
-            if(childEl.length) return childEl.length;
-
-            childEl.childNodes.forEach(function(childEl) {
-                result += countContentLengthOfChildren(childEl, endChild);
-            });
-
-            return result;
+            treeWalker.nextNode();
         }
 
-        return countContentLengthOfChildren(element, endChild);
+        return length;
+    },
+
+    // rightMatch: when root element contains: '<p>Hello</p><p>World</p>' and contentLength is '5'
+    //             it is not clear which element to return, if rightMatch is true then '<p>World</p>' will
+    //             be returned, '<p>Hello</p>' otherwise
+    _getElementByContentLength: function(rootEl, contentLength, rightMatch) {
+        var treeWalker = document.createTreeWalker(rootEl),
+            currentEl,
+            length = 0,
+            currentElLength;
+
+        while(currentEl = treeWalker.currentNode) {
+            currentElLength = this._getElementsContentLengthIncrement(currentEl);
+
+            if((length + currentElLength) < contentLength) {
+                length += currentElLength;
+            } else if((length + currentElLength) === contentLength) {
+                return rightMatch ? (treeWalker.nextNode() || currentEl) : currentEl;
+            } else {
+                return currentEl;
+            }
+
+            treeWalker.nextNode();
+        }
+
+        return null;
     },
 
     restoreSelection: function(rootEl) {
+        if(this.rangeStart && this.rangeEnd) {
+
+            var range,
+                selection = window.getSelection(),
+                startNode = this._getElementByContentLength(rootEl, this.rangeStart, this.rangeStartAtZeroOffset),
+                endNode = this._getElementByContentLength(rootEl, this.rangeEnd, this.rangeEndAtZeroOffset),
+                startOffset = this.rangeStart - this._getContentLengthOfDOMElement(rootEl, startNode),
+                endOffset = this.rangeEnd - this._getContentLengthOfDOMElement(rootEl, endNode);
+
+            range = document.createRange();
+            range.setStart(startNode, startOffset);
+            range.setEnd(endNode, endOffset);
+            selection.removeAllRanges();
+            selection.addRange(range);
+        }
     },
 
     // text|text
@@ -48,8 +85,8 @@ Caret.prototype = {
         if(selection.anchorNode && selection.focusNode) {
             this.rangeStart = this._getContentLengthOfDOMElement(rootEl, selection.anchorNode) + selection.anchorOffset;
             this.rangeEnd = this._getContentLengthOfDOMElement(rootEl, selection.focusNode) + selection.focusOffset;
+            this.rangeStartAtZeroOffset = selection.anchorOffset == 0;
+            this.rangeEndAtZeroOffset = selection.focusOffset == 0;
         }
-
-        console.log(this.rangeStart)
     }
 }
