2fb0afee11ed01e2e7d167a9b097e2e060869151
Wolf Oliver <owolf@specify.io>
2017-05-14 16:57:44 +0200

    slight refactoring in caret.js

--- js_sdk/caret.js
+++ js_sdk/caret.js
@@ -8,27 +8,22 @@ Caret.prototype = {
 
     // endChild is optional, if given it must be child element
     // of the element passed in the first parameter
+    //TODO: evaluate whether innerText can help to simpify this (seems not to be helpfull because it does not repsect img tags)
     _getContentLengthOfDOMElement: function(element, endChild) {
 
         var stopCounting = false;
 
-        if (element === endChild) return 0;
-
         function countContentLengthOfChildren(childEl, endChild) {
             var result = 0;
 
-            childEl.childNodes.forEach(function(childEl) {
-                if (childEl === endChild) {
-                    stopCounting = true;
-                }
+            if(childEl === endChild) stopCounting = true;
+            if(stopCounting) return 0;
 
-                if (childEl.length && !stopCounting) {
-                    result += childEl.length;
-                } else if(childEl.tagName === 'BR' && !stopCounting) {
-                    result += 1;
-                } else if (!stopCounting) {
-                    result += countContentLengthOfChildren(childEl, endChild);
-                }
+            if(['BR', 'IMG'].includes(childEl.tagName)) return 1;
+            if(childEl.length) return childEl.length;
+
+            childEl.childNodes.forEach(function(childEl) {
+                result += countContentLengthOfChildren(childEl, endChild);
             });
 
             return result;
@@ -37,16 +32,24 @@ Caret.prototype = {
         return countContentLengthOfChildren(element, endChild);
     },
 
+    restoreSelection: function(rootEl) {
+    },
 
+    // text|text
+    // <div>text|</div><div>text</div>
+    // <div>text</div><div>|text<,/div>
+    // text|<br/><br/><br/>text
+    // text<br/>|<br/><br/>text
+    // text<br/><br/>|<br/>text
+    // text<br/><br/><br/>|text
+    // Preformatted stuff (root element includes <pre> tags)
     saveSelection: function(rootEl) {
         var selection = window.getSelection();
         if(selection.anchorNode && selection.focusNode) {
             this.rangeStart = this._getContentLengthOfDOMElement(rootEl, selection.anchorNode) + selection.anchorOffset;
             this.rangeEnd = this._getContentLengthOfDOMElement(rootEl, selection.focusNode) + selection.focusOffset;
         }
-    },
 
-    restoreSelection: function(rootEl) {
-
+        console.log(this.rangeStart)
     }
 }
