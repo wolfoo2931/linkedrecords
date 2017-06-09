0b7c98dc62516ea1ec25b98e4bd472cad4f2133f
Wolf Oliver <owolf@specify.io>
2017-06-09 22:00:00 +0000

    start to implement editor object

---
+++ js_sdk/editor.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var $ = require('jquery'),
+    Caret = require('../js_sdk/caret');
+
+var Editor = function(domId) {
+    var self = this;
+    this.domId = domId;
+    this.contentElement = document.getElementById(domId);
+    this.caret = new Caret();
+
+    this.subscribe(function() {
+        self.cleanUpContent();
+    });
+}
+
+Editor.prototype = {
+    subscribe: function(callback) {
+        var self = this;
+        $('#value').on('input', function() {
+            callback(self._cleanHTML(self.contentElement.innerHTML));
+        });
+    },
+
+    cleanUpContent: function() {
+        this.caret.saveSelection(this.contentElement);
+        this.contentElement.innerHTML = this._cleanHTML(this.contentElement.innerHTML);
+        this.caret.restoreSelection(this.contentElement);
+    },
+
+    setContent: function(content) {
+        this.caret.saveSelection(this.contentElement);
+        this.contentElement.innerHTML = this._cleanHTML(content);
+        this.caret.restoreSelection(this.contentElement);
+    },
+
+    _cleanHTML: function(html) {
+        html = html.replace(/<br\/>/g, '');
+        html = html.replace(/<br>/g, '');
+        html = html.replace(/<b><\/b>/g, '');
+        html = html.replace(/<i><\/i>/g, '');
+        html = html.replace(/<div>\s*<\/div>/g, '');
+        html = html.replace(/<div>/g, '<p>');
+        html = html.replace(/<\/div>/g, '</p>');
+        html = html.replace(/<p><\/p>(<p><\/p>)+/g, '<p> </p>');
+        return html;
+    }
+}
+
+module.exports = Editor;
