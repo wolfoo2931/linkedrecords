01bbb1556fe3f6147315768e91c550f290d8c886
Oliver Wolf <owolf@specify.io>
2022-06-19 18:31:24 +0200

    setup typescript for nodejs

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 const path = require('path');
+const WebpackShellPlugin = require('webpack-shell-plugin-next');
+const nodeExternals = require('webpack-node-externals');
+const NODE_ENV = 'development';
 
 const exampleFrontned = {
     entry: path.join(__dirname, 'staticfiles', 'example.ts'),
     target: 'web',
-    mode: 'development',
+    mode: NODE_ENV,
     devtool: 'inline-source-map',
+    watch: NODE_ENV === 'development',
     output: {
         filename: 'example.packaged.js',
         path: path.join(__dirname, 'staticfiles')
@@ -26,6 +30,38 @@ const exampleFrontned = {
     optimization: {
         minimize: false
     }
-}
+};
 
-module.exports = [ exampleFrontned ]
+const main = {
+    entry: './index.ts',
+    mode: NODE_ENV,
+    target: 'node',
+    externals: [ nodeExternals() ],
+    watch: NODE_ENV === 'development',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'index.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+            },
+        ]
+    },
+    plugins: [
+        new WebpackShellPlugin({
+            onBuildEnd: {
+                scripts: ['npm run run:dev'],
+                blocking: false,
+                parallel: true
+            }
+        })
+    ]
+};
+
+module.exports = [ main, exampleFrontned ]
