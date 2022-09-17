a1dd44a4c152dafbe1f599c3ae07edc3f8cbf97c
Oliver Wolf <owolf@specify.io>
2022-09-17 17:17:17 +0000

    add middleware to init attribute for a given request

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-import { LongTextAttribute, PsqlStorage } from '../attributes/long_text/server';
+import attributeMiddleware, { getAttributeByParams } from './middleware/attribute';
 import { Server } from 'http';
 import express from 'express';
 import faye from 'faye';
@@ -10,50 +10,28 @@ import 'dotenv/config';
 const app = express();
 const server = new Server(app);
 const bayeux = new faye.NodeAdapter({mount: '/bayeux', timeout: 45});
-const storage = new PsqlStorage();
 
 bayeux.attach(server);
 
 app.use(cors());
-app.use(express.json())
-
-function getAttributeClassByAttributeId(id: string) : any {
-    const attributeTypes = [ LongTextAttribute ]
-
-    const [attributeTypePrefix] = id.split('-');
-    const attributeClass = attributeTypes.find(c => c.DATA_TYPE_PREFIX === attributeTypePrefix);
-
-    return attributeClass;
-}
+app.use(express.static('staticfiles'));
+app.use(express.json());
+app.use(attributeMiddleware);
 
 bayeux.getClient().subscribe('/uncommited/changes/attribute/*', async ({ id, change, actorId, clientId }) => {
-    const attributeClass = getAttributeClassByAttributeId(id);
+
+    const attribute = getAttributeByParams({ params: { id, actorId, clientId } });
 
     try {
-        if(!attributeClass) {
-            throw `Server is unkown of Attribute Type Prefix for id ${id}`
-        }
-
-        const starTime = Date.now();
-        const attribute = new attributeClass(id, clientId, actorId, storage);
         const commitedChange = await attribute.change(change.changeset, change.parentVersion);
         bayeux.getClient().publish('/changes/attribute/' + id, commitedChange);
-        console.log('Change attribute (' + id + ') finished in ', Date.now() - starTime);
     } catch(ex) {
         console.error(`error in /uncommited/changes/attribute/${id}`, ex);
     }
 });
 
 app.get('/attributes/:id', async (req, res) => {
-    const attributeClass = getAttributeClassByAttributeId(req.params.id);
-
-    if(!attributeClass) {
-        res.status(404).send({error: `Server is unkown of Attribute Type Prefix for id ${req.params.id}`});
-        return;
-    }
-
-    const attribute = new attributeClass(req.params.id, req.params.clientId, req.params.actorId, storage);
-    const result = await attribute.get();
+    const result = await req.attribute.get();
 
     if(result instanceof Error) {
         console.error(`error in GET /attributes/${req.params.id}`, result.message);
@@ -64,16 +42,8 @@ app.get('/attributes/:id', async (req, res) => {
 });
 
 app.post('/attributes/:id', async (req, res) => {
-    const attributeClass = getAttributeClassByAttributeId(req.params.id);
-
-    if(!attributeClass) {
-        res.status(404).send({error: `Server is unkown of Attribute Type Prefix for id ${req.params.id}`});
-        return;
-    }
-
-    const attribute = new attributeClass(req.params.id, req.body.clientId, req.body.actorId, storage);
-    await attribute.create(req.body.value);
-    const result = await attribute.get();
+    await req.attribute.create(req.body.value);
+    const result = await req.attribute.get();
 
     if(result instanceof Error) {
         console.error(`error in GET /attributes/${req.params.id}`, result.message);
@@ -82,8 +52,6 @@ app.post('/attributes/:id', async (req, res) => {
         res.send(result);
     }
 });
-
-app.use(express.static('staticfiles'));
 
 server.listen(process.env.PORT || 3000);
 
