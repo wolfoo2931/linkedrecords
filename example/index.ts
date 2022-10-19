635ba2bbd62855fe9ecc650b3f10556f3a045c9d
Oliver Wolf <owolf@specify.io>
2022-10-19 17:38:42 +0000

    adjust example to use facts for data retrival

--- example/index.ts
+++ example/index.ts
@@ -3,36 +3,46 @@ import Editor from 'structured-text-editor/src/editor';
 import LinkedRecords from '../src/browser_sdk/index';
 import LongTextChange from '../src/attributes/long_text/long_text_change';
 import KeyValueChange from '../src/attributes/key_value/key_value_change';
-
-const stores = {
-  'content': 'l-03069649-ae59-4633-a199-c85993010158',
-  'reference': 'kv-89193aea-4488-4dd7-a0fc-de80d68f4666',
-  'reference_sources': 'kv-0466df56-4e36-4531-8876-600853caffe7'
-}
+import LongTextAttribute from '../src/attributes/long_text/client';
+import KeyValueAttribute from '../src/attributes/key_value/client';
 
 document.addEventListener('DOMContentLoaded', async (event) => {
   const editor = new Editor('value');
   const linkedRecords = new LinkedRecords(new URL('http://localhost:3000'));
-  const contentAttribute = await linkedRecords.Attribute.find(stores.content); // await linkedRecords.Attribute.create('longText', '<p>inital</p>');
-  const referencesAttribute = await linkedRecords.Attribute.find(stores.reference);
+
+  // const content = await linkedRecords.Attribute.create('longText', 'inital');
+  // console.log('New ContentId', content.id);
+  // const contentId = content.id;
+
+  const contentId = 'l-39d5ab07-b571-4d3b-abf0-2a5974fe41df';
+
+  let { content: contentAttribute, refernces: [ referencesAttribute ] } = <{
+    content: LongTextAttribute,
+    refernces: KeyValueAttribute[],
+    referenceSources: KeyValueAttribute[]
+  }> <unknown> await linkedRecords.Attribute.findAll({
+    content: contentId,
+    refernces: [
+      ['isA', 'referenceStore'],
+      ['belongsTo', contentId],
+    ],
+    referenceSources: [
+      ['isA', 'referenceSourceStore'],
+      ['belongsTo', contentId],
+      ['belongsTo', 'usr-xx'],
+    ]
+  });
+
+  if (!referencesAttribute) {
+    referencesAttribute = await linkedRecords.Attribute.create('keyValue', {}) as KeyValueAttribute;
+    await linkedRecords.Fact.createAll([
+      [referencesAttribute.id, 'isA', 'referenceStore'],
+      [referencesAttribute.id, 'belongsTo', contentId]
+    ]);
+  }
 
   editor.setContent(await contentAttribute.getValue());
   editor.addReferenceData(await referencesAttribute.getValue());
-
-  // const { content: [ contentAttribute ], refernces: [ referencesAttribute ] } = await linkedRecords.Attribute.findAll({
-  //   content: [
-  //     ['identifiedBy', 'l-03069649-ae59-4633-a199-c85993010158']
-  //   ],
-  //   refernces: [
-  //     ['isA', 'referenceStore'],
-  //     ['belongsTo', 'l-03069649-ae59-4633-a199-c85993010158']
-  //   ],
-  //   referenceSources: [
-  //     ['isA', 'referenceSourceStore'],
-  //     ['belongsTo', 'l-03069649-ae59-4633-a199-c85993010158'],
-  //     ['belongsTo', 'usr-xx']
-  //   ]
-  // });
 
   referencesAttribute.subscribe(async (changeset) => {
     const newData = {};
@@ -45,7 +55,7 @@ document.addEventListener('DOMContentLoaded', async (event) => {
   });
 
   editor.subscribeReferenceInsertion(async (key, value) => {
-    referencesAttribute.change(new KeyValueChange([{ key, value }]));
+    referencesAttribute!.change(new KeyValueChange([{ key, value }]));
   });
 
   contentAttribute.subscribe(async (changeset, changeInfo) => {
