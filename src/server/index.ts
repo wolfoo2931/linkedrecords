b3cb83addaa584401436b8feb8743f0ec9cb0d8d
Oliver Wolf <owolf@specify.io>
2022-10-21 05:01:23 +0000

    allow to implement authorization logic in extra file

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,37 +1,17 @@
 import { Server } from 'http';
 import express from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
-import cookieParser from 'cookie-parser';
-import serverSentEvents from '../../lib/server-side-events/server';
-import attributeMiddleware from './middleware/attribute';
-import Fact from '../facts/server';
-import factsController from './controllers/facts_controller';
-import attributesController from './controllers/attributes_controller';
+import createApp from './linkedrecords';
 
-// import authentication from './middleware/authentication';
-import 'dotenv/config';
+createApp({
+  isAuthorizedToCreateAttribute: () => true,
+  isAuthorizedToReadAttribute: () => true,
+  isAuthorizedToUpdateAttribute: () => true,
+  isAuthorizedToCreateFacts: () => true,
+  isAuthorizedToReadFacts: () => true,
+  isAuthorizedToUpdateFacts: () => true,
+}).then((app) => {
+  app.use('/example', express.static('example'));
 
-const app = express();
-const server = new Server(app);
-Fact.initDB();
-
-app.use('/example', express.static('example'));
-app.use(morgan('tiny', { skip: (req) => req.method === 'OPTIONS' }));
-app.use(cors({ origin: '*' }));
-app.use(cookieParser(process.env['AUTH_COOKIE_SIGNING_SECRET']));
-app.use(express.json());
-app.use(serverSentEvents());
-// app.use(authentication());
-app.use('/attributes', attributeMiddleware());
-
-app.post('/attributes/:attributeId', attributesController.create);
-app.get('/attributes/:id', attributesController.get);
-app.get('/attributes/:attributeId/changes', attributesController.subsribe);
-app.patch('/attributes/:attributeId', attributesController.update);
-
-app.get('/facts', factsController.index);
-app.post('/facts', factsController.create);
-app.delete('/facts', factsController.deleteAll);
-
-server.listen(process.env['PORT'] || 3000);
+  const server = new Server(app);
+  server.listen(process.env['PORT'] || 3000);
+});
