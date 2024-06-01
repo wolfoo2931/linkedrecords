518de5f98fbc9a8bc47fa4d1f9e6bbd0a2a18b2a
Oliver Wolf <owolf@specify.io>
2024-06-01 14:02:46 +0200

    make sure the latest version is running as server

--- bin/test.wdio.sh
+++ bin/test.wdio.sh
@@ -9,8 +9,16 @@ npx webpack --config ./specs.wdio/testapp/webpack.config.js
 
 bin/testserver.sh&
 SERVER_PID=$!
-echo "Node server is running on with PID ${SERVER_PID}"
+
 sleep 2
+
+if ! ps -p $SERVER_PID; then
+  echo "Node test server is NOT running!!! Maybe the server is running already and blocking the port. Check for 'ps aux | grep testserver' and terminate it."
+  exit 1
+fi
+
+echo "Node server is running on with PID ${SERVER_PID}"
+
 npx wdio run ./wdio.conf.ts
 succ=$?
 kill $SERVER_PID
--- bin/testserver.sh
+++ bin/testserver.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+pkill -f '.*node.*testserver.js.*'
 node --enable-source-maps specs.wdio/testapp/testserver.js | bin/plogs.js
