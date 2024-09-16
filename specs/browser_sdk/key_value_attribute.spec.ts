406423dc2c2693dfab77601a119aa329b31ff91e
Oliver Wolf <owolf@specify.io>
2024-09-16 11:36:57 +0200

    try to fix tests on github actions runner

--- specs/browser_sdk/key_value_attribute.spec.ts
+++ specs/browser_sdk/key_value_attribute.spec.ts
@@ -227,7 +227,7 @@ describe('Key Value Attributes', () => {
   });
 
   describe('attribute.change()', () => {
-    it.only('allows to change only parts of the document when the value is a JSON object', async () => {
+    it('allows to change only parts of the document when the value is a JSON object', async () => {
       const [clientA] = await createClient();
       const [clientB] = await createClient();
 
