9e63d7dc6a2baeb50cd503ce834e70be33960f70
Oliver Wolf <owolf@specify.io>
2025-03-29 15:59:59 +0200

    try to fix broken tests

--- src/server/quota/index.ts
+++ src/server/quota/index.ts
@@ -156,9 +156,12 @@ export default class Quota {
   }
 
   public async getTotalStorageAvailable(): Promise<number> {
-    const data = await this.pool.query('SELECT total_storage_available FROM quota_events WHERE node_id=$1 AND valid_from > NOW() ORDER BY id DESC LIMIT 1', [this.nodeId]);
+    const data = await this.pool.query('SELECT total_storage_available FROM quota_events WHERE node_id=$1 AND valid_from >= $1 ORDER BY id DESC LIMIT 1', [
+      this.nodeId,
+      new Date(),
+    ]);
 
-    console.log('xxxxxxxxxxxx', data);
+    console.log('xxxxxxxxxxxx', new Date());
     console.log('all', await this.pool.query('SELECT * FROM quota_events'));
 
     if (data.rows.length && Number.parseInt(data.rows[0].total_storage_available, 10)) {
