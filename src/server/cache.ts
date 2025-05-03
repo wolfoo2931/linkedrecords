d08e6e55bd98f9884021c094b0221cfd674c7fe7
Wolf Oliver <owolf@specify.io>
2025-05-03 17:01:34 +0200

    cache fact box lookups (#63)
    * cache fact box lookups
    
    * fix redis connection undefined

--- src/server/cache.ts
+++ src/server/cache.ts
@@ -1,9 +1,48 @@
+import { createClient } from 'redis';
+
 const cache: Map<any, any> = new Map();
+
+const redisConnectionsPromise = {};
+
+function getRedisConnectionPromise(connectionName) {
+  if (!process.env['REDIS_HOST']) {
+    return Promise.resolve(undefined);
+  }
+
+  if (redisConnectionsPromise[connectionName]) {
+    return redisConnectionsPromise[connectionName];
+  }
+
+  const redisClient = createClient({
+    username: process.env['REDIS_USERNAME'],
+    password: process.env['REDIS_PASSWORD'],
+    socket: {
+      host: process.env['REDIS_HOST'],
+      port: 6379,
+    },
+  });
+
+  redisConnectionsPromise[connectionName] = redisClient.connect();
+  return redisConnectionsPromise[connectionName];
+}
+
+getRedisConnectionPromise('sub').then((redisClient) => {
+  if (redisClient) {
+    redisClient.subscribe('invalidate_local_cache_entry', (message) => {
+      cache.delete(message);
+    });
+  }
+});
 
 export default {
   get: (key: string) => cache.get(key),
   set: (key: string, value: any) => cache.set(key, value),
   invalidate: (key: string) => {
     cache.delete(key);
+    getRedisConnectionPromise('pub').then((redisClient) => {
+      if (redisClient) {
+        redisClient.publish('invalidate_local_cache_entry', key);
+      }
+    });
   },
 };
