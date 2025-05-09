56c962b4d7fdd8589fb4fce1a59d56b1800303b8
Oliver Wolf <owolf@specify.io>
2025-05-09 13:59:15 +0200

    test and fix edge case when chaching access lookups

--- src/facts/server/auth_cache.ts
+++ src/facts/server/auth_cache.ts
@@ -3,6 +3,7 @@ import Fact from '.';
 import IsLogger from '../../../lib/is_logger';
 import cache from '../../server/cache';
 import { Role } from './authorization_sql_builder';
+import FactBox from './fact_box';
 
 export default class AuthCache {
   static async hasCachedAccess(
@@ -10,13 +11,19 @@ export default class AuthCache {
     roles: Role[],
     attributeId: string,
     logger: IsLogger,
-  ) {
-    logger.info(`cache access cache for: ${userid}, ${roles}, ${attributeId}`);
-
+  ): Promise<boolean> {
     const roleKey = roles.sort().join(':');
     const allowedRoleCombos = this.getCachedRoleCombos(userid, attributeId);
 
-    return allowedRoleCombos.includes(roleKey);
+    const cacheHit = allowedRoleCombos.includes(roleKey);
+
+    if (cacheHit) {
+      logger.info(`cache hit for access request: ${userid}, ${roles}, ${attributeId}`);
+    } else {
+      logger.info(`cache MISS!, ${roles}, ${allowedRoleCombos.join(' and ')}`);
+    }
+
+    return cacheHit;
   }
 
   static async onFactDeletion(
@@ -29,18 +36,18 @@ export default class AuthCache {
 
     if (fact.subject.startsWith('us-')) {
       cache.invalidate(fact.subject);
+      return;
     }
 
-    // TODO
-    // FIND test cases
+    const users = await FactBox.getAllAssociatedUsers(fact.subject, logger);
+    users.forEach((uid) => cache.invalidate(uid));
   }
 
   private static getCachedRoleCombos(
     userid: string,
     attributeId: string,
   ) {
-    const rawUserCache = cache.get(userid);
-    const userCache = rawUserCache ? JSON.parse(rawUserCache) : {};
+    const userCache = cache.get(userid) || {};
 
     if (!userCache[attributeId]) {
       userCache[attributeId] = [];
@@ -61,8 +68,7 @@ export default class AuthCache {
 
     logger.info(`cache access cache for: ${userid}, ${roles}, ${attributeId}`);
 
-    const rawUserCache = cache.get(userid);
-    const userCache = rawUserCache ? JSON.parse(rawUserCache) : {};
+    const userCache = cache.get(userid) || {};
     const roleKey = roles.sort().join(':');
 
     if (!userCache[attributeId]) {
@@ -71,7 +77,26 @@ export default class AuthCache {
 
     if (!userCache[attributeId].includes(roleKey)) {
       userCache[attributeId].push(roleKey);
-      cache.set(userid, JSON.stringify(userCache));
+      cache.set(userid, userCache);
+    }
+  }
+
+  static async cacheQueryResult(actorId: string, result: any, logger: IsLogger,) {
+    if (Array.isArray(result)) {
+      result.map((attr) => AuthCache.cache(actorId, ['reader'], attr.id, logger));
+    } else if (result) {
+      const allResults: { id: string }[][] = Object.values(result);
+      allResults.map((partialResult: any) => {
+        if (Array.isArray(partialResult)) {
+          return partialResult.map((attr) => AuthCache.cache(actorId, ['reader'], attr.id, logger));
+        }
+
+        if (partialResult && partialResult.id) {
+          return AuthCache.cache(actorId, ['reader'], partialResult.id, logger);
+        }
+
+        return undefined;
+      });
     }
   }
 }
