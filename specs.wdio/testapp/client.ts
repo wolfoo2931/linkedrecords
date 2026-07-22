0b67b8d945131ff9f47d65e8f49390e7764b5af4
Oliver Wolf <owolf@specify.io>
2026-07-22 19:59:27 +0200

    refactor client creation in test app

--- specs.wdio/testapp/client.ts
+++ specs.wdio/testapp/client.ts
@@ -1,4 +1,5 @@
 import LinkedRecords from '../../src/browser_sdk';
+import { OIDCConfig } from '../../src/browser_sdk/oidc';
 
 // Baked in at build time via webpack's DefinePlugin (see webpack.config.js):
 // TESTAPP_AUTH_MODE mirrors the TEST_AUTH_MODE env variable of the test run,
@@ -8,25 +9,19 @@ declare const AUTH_CLIENT_ID: string;
 declare const AUTH_ISSUER_BASE_URL: string;
 
 async function init() {
-  let client: LinkedRecords;
+  const lrServerUrl = new URL('http://localhost:3000');
+  const config: OIDCConfig | undefined = TESTAPP_AUTH_MODE === 'confidential' ? undefined : {
+    authority: AUTH_ISSUER_BASE_URL,
+    client_id: AUTH_CLIENT_ID,
+    redirect_uri: `${window.location.origin}/callback`,
+    audience: AUTH_TOKEN_AUDIENCE || undefined,
+    scope: 'openid profile email',
+    response_type: 'code',
+    useSessionStorage: true,
+  };
 
-  if (TESTAPP_AUTH_MODE === 'public') {
-    client = new LinkedRecords(
-      new URL('http://localhost:3000'),
-      {
-        authority: AUTH_ISSUER_BASE_URL,
-        client_id: AUTH_CLIENT_ID,
-        redirect_uri: `${window.location.origin}/callback`,
-        audience: AUTH_TOKEN_AUDIENCE || undefined,
-        scope: 'openid profile email',
-        response_type: 'code',
-        useSessionStorage: true,
-      },
-    );
-  } else {
-    client = new LinkedRecords(new URL('http://localhost:3000'));
-    await client.ensureUserIdIsKnown();
-  }
+  const client = new LinkedRecords(lrServerUrl, config, true, true);
+  await client.ensureUserIdIsKnown();
 
   // when the majority of the test were written
   // the default handler which throws an exception
